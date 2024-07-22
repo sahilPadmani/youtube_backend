9ed1987d68110e61920f01031c3477de25919639
sahilpadmani <sahilpadmani616@gmail.com>
2024-07-23 00:01:27 +0530

    part 1 finsh

--- src/index.js
+++ src/index.js
@@ -3,14 +3,13 @@ import dotenv from "dotenv";
 import ConnectDB from "./db/index.js";
 import app from "./app.js";
 
-dotenv.config({ path: './env' });
+dotenv.config({ path: './.env' });
 
 ConnectDB()
     .then(() => {
         app.on("error", error => {
             throw error;
         });
-
         app.listen(process.env.PORT || 8000, () => {
             console.log(`Successfully connected to port ${process.env.PORT || 8000}`);
         });
